8e558cb7cd4a47882eb76042e2104d6bed7c3611
sushant chapagain <sreddevil12@gmail.com>
2023-11-20 18:46:22 +0545

    till fetching cabins

--- 14-the-wild-oasis/src/App.jsx
+++ 14-the-wild-oasis/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import Dashboard from './pages/Dashboard';
 import Account from './pages/Account';
@@ -9,25 +12,34 @@ import PageNotFound from './pages/PageNotFound';
 import Settings from './pages/Dashboard';
 import Users from './pages/Users';
 import GlobalStyles from './styles/GlobalStyle';
+import AppLayout from './ui/AppLayout';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 60 * 1000 } },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="account" element={<Account />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="account" element={<Account />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<Users />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- 14-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 14-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,4 +1,10 @@
-import styled from "styled-components";
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import styled from 'styled-components';
+import { getCabins } from '../../services/apiCabins';
+import CabinRow from './CabinRow';
+
+import Spinner from '../../ui/Spinner';
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -23,3 +29,33 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  const {
+    data: cabins,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['cabins'],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price </div>
+        <div>Discount</div>
+      </TableHeader>
+      {cabins?.map((cabin) => (
+        <CabinRow key={cabin.id} cabin={cabin} />
+      ))}
+    </Table>
+  );
+}
+
+export default CabinTable;
