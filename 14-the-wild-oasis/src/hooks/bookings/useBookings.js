8a175576657a33b034a9255d16be94b901ed9211
sushant chapagain <sreddevil12@gmail.com>
2023-11-29 15:57:03 +0545

    tlll sorting

--- 14-the-wild-oasis/src/hooks/bookings/useBookings.js
+++ 14-the-wild-oasis/src/hooks/bookings/useBookings.js
@@ -1,10 +1,30 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 
+import { useSearchParams } from 'react-router-dom';
+
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get('status');
+
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+  // { field: 'totalPrice', value: 5000, method: 'gte' };
+
+  // SORTBY
+  const sortByValue = searchParams.get('sortBy') || 'startDate-desc';
+
+  const [field, direction] = sortByValue.split('-');
+
+  const sortBy = { field, direction };
+
   const { data: bookings, isLoading } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { bookings, isLoading };
--- 14-the-wild-oasis/src/services/apiBookings.js
+++ 14-the-wild-oasis/src/services/apiBookings.js
@@ -1,13 +1,21 @@
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from('bookings')
     //get or populate data from relationship table.
     .select(
       'id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)'
     );
+
+  if (filter) query[filter.method || 'eq'](filter.field, filter.value);
+
+  if (sortBy)
+    query.order(sortBy.field, { ascending: sortBy.direction === 'asc' });
+
+  const { data, error } = await query;
+
   if (error) {
     console.error(error);
     throw new Error('Bookings could not be loaded');
