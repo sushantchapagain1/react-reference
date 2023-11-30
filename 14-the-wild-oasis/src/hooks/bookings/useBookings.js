d2fb8354c40a113788d4e10bda05ad7421cd39cc
sushant chapagain <sreddevil12@gmail.com>
2023-11-30 10:34:17 +0545

    tlll prefetching

--- 14-the-wild-oasis/src/hooks/bookings/useBookings.js
+++ 14-the-wild-oasis/src/hooks/bookings/useBookings.js
@@ -1,12 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 
 import { useSearchParams } from 'react-router-dom';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
-  // FILTER
+  //A.-------------------- FILTER-------------------
   const filterValue = searchParams.get('status');
 
   const filter =
@@ -15,17 +17,36 @@ export function useBookings() {
       : { field: 'status', value: filterValue };
   // { field: 'totalPrice', value: 5000, method: 'gte' };
 
-  // SORTBY
+  //B. -------------------- SORTBY-------------------
   const sortByValue = searchParams.get('sortBy') || 'startDate-desc';
 
   const [field, direction] = sortByValue.split('-');
 
   const sortBy = { field, direction };
 
-  const { data: bookings, isLoading } = useQuery({
-    queryKey: ['bookings', filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+  // C.-------------------- PAGINATION-------------------
+  const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
+
+  // D.-------------------- QUERY-------------------
+  const { isLoading, data: { data: bookings, count } = {} } = useQuery({
+    queryKey: ['bookings', filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { bookings, isLoading };
+  // E.-------------------- PRE-FETCH-------------------
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+
+  return { isLoading, bookings, count };
 }
--- 14-the-wild-oasis/src/services/apiBookings.js
+++ 14-the-wild-oasis/src/services/apiBookings.js
@@ -1,12 +1,15 @@
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
-export async function getBookings({ filter, sortBy }) {
+import { PAGE_SIZE } from '../utils/constants';
+
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from('bookings')
     //get or populate data from relationship table.
     .select(
-      'id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)'
+      'id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)',
+      { count: 'exact' }
     );
 
   if (filter) query[filter.method || 'eq'](filter.field, filter.value);
@@ -14,13 +17,19 @@ export async function getBookings({ filter, sortBy }) {
   if (sortBy)
     query.order(sortBy.field, { ascending: sortBy.direction === 'asc' });
 
-  const { data, error } = await query;
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error('Bookings could not be loaded');
   }
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
