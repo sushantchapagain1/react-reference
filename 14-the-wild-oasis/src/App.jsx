e3ac245a637b300cd71e8bd41585d2c6b9005cf8
sushant chapagain <sreddevil12@gmail.com>
2023-12-03 12:54:40 +0545

    replace bug

--- 14-the-wild-oasis/src/App.jsx
+++ 14-the-wild-oasis/src/App.jsx
@@ -24,25 +24,10 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <DarkModeProvider>
-        <Toaster
-          position="top-center"
-          gutter={12}
-          containerStyle={{ margin: '4px' }}
-          toastOptions={{
-            success: { duration: 3000 },
-            error: { duration: 5000 },
-            style: {
-              fontSize: '16px',
-              maxWidth: '500px',
-              padding: '16px 24px',
-              backgroundColor: 'var(--color-grey-0)',
-              color: 'var(--color-grey-700)',
-            },
-          }}
-        />
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
+
         <GlobalStyles />
         <BrowserRouter>
           <Routes>
@@ -53,23 +38,44 @@ function App() {
                 </ProtectedRoute>
               }
             >
-              <Route index element={<Navigate to="dashboard" />} />
+              <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
               <Route path="bookings/:bookingId" element={<Booking />} />
               <Route path="check-in/:bookingId" element={<CheckIn />} />
-              <Route path="account" element={<Account />} />
               <Route path="cabins" element={<Cabins />} />
-              <Route path="settings" element={<Settings />} />
               <Route path="users" element={<Users />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="account" element={<Account />} />
             </Route>
 
             <Route path="login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
-      </DarkModeProvider>
-    </QueryClientProvider>
+
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px 24px',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
